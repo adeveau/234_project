b48bdf025687dbaf405199a511fd03972f203644
Andrew Deveau <adeveau9@gmail.com>
2018-03-11 20:58:05 -0700

    add deepq

---
+++ src/deepq.py
@@ -0,0 +1,8 @@
+from MultiEnv import make_multienv
+from baselines import deepq
+from gym.envs import toy_text
+
+model = deepq.models.mlp([64])
+env = make_multienv([toy_text.NChainEnv(slip = x) for x in (0,1)], 10)
+
+act = deepq.learn(env, q_func = model, max_timesteps=100, buffer_size = 10)
